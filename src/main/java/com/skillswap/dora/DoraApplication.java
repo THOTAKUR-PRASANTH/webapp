7126515e5b2767af15d75ca4ed23bc30b0a9bee6
THOTAKURA-PRASANTH <211fa04025@gmail.com>
2025-12-25 18:12:00 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/skillswap/dora/DoraApplication.java
@@ -0,0 +1,13 @@
+package com.skillswap.dora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoraApplication.class, args);
+	}
+
+}
